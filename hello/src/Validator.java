e2fccebfa886b86ad0758aad51febad08162e1f7
Lawson <145367908+Zaeer23@users.noreply.github.com>
2025-03-13 12:32:41 -0500

    Delete hello/src directory

--- hello/src/Validator.java
+++
@@ -1,8 +0,0 @@
-import java.util.Scanner;
-class Validator {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.print("Enter a phone number: ");
-        System.out.println(input.nextLine().matches( "^1? ?(\\(\\d{3}\\)|\\d{3})[- ]?\\d{3}[- ]?\\d{4}$"));
-    }
-}
